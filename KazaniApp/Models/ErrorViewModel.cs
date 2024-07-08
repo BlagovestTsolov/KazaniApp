d57cb84658bbd82a26ece43cddfd2d9e0ca6b2c0
BlagovestTsolov <bobytsolov@gmail.com>
2024-07-08 15:16:37 +0300

    Add project files.

---
+++ KazaniApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KazaniApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
