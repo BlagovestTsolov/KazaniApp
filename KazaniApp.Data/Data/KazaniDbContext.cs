f0d0579ed9dc99a9d5afa2681aad6f59fb055104
BlagovestTsolov <bobytsolov@gmail.com>
2024-07-08 20:33:44 +0300

    project separation and custom db class created

---
+++ KazaniApp.Data/Data/KazaniDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KazaniApp.Data.Data
+{
+    public class KazaniDbContext : IdentityDbContext
+    {
+        public KazaniDbContext(DbContextOptions<IdentityDbContext> options)
+            : base(options){}
+
+
+    }
+}
--- KazaniApp/Extensions/ServiceExtenstions.cs
+++ KazaniApp/Extensions/ServiceExtenstions.cs
@@ -1,4 +1,4 @@
-﻿using KazaniApp.Data;
+﻿using KazaniApp.Data.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,7 +17,7 @@ namespace KazaniApp.Extensions
         {
             var connectionString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<KazaniDbContext>(options =>
                 options.UseSqlServer(connectionString));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
@@ -35,7 +35,7 @@ namespace KazaniApp.Extensions
                     options.Password.RequireUppercase = false;
                     options.Password.RequiredLength = 5;
                 })
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<KazaniDbContext>();
 
             return services;
         }
