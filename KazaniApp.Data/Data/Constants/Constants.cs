c595ee5dd3541daac4fec52c382328697c02ab21
BlagovestTsolov <bobytsolov@gmail.com>
2024-07-08 20:51:34 +0300

    data constants added

---
+++ KazaniApp.Data/Data/Constants/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace KazaniApp.Data.Data.Constants
+{
+    public static class Constants
+    {
+        const int ProductDescriptionMaxLength = 250;
+        const int ProductDescriptionMinLength = 10;
+    }
+}
